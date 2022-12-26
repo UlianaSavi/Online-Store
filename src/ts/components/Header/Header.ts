9dddb5f76c4c623ccf3463a45a9d48093126eba5
UlianaSavi <uljasha.sav@gmail.com>
2022-12-26 18:15:58 +0600

    feat: translate header to english

--- src/ts/components/Header/Header.ts
+++ src/ts/components/Header/Header.ts
@@ -45,7 +45,7 @@ export class Header {
               classNames: 'search__input',
               dataAttr: [
                 ['type', 'text'],
-                ['placeholder', 'Поиск']
+                ['placeholder', 'Search']
               ]
             }),
             create({
