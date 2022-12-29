478d6854143a06e4ce573df027a081db4d7c9adc
UlianaSavi <uljasha.sav@gmail.com>
2022-12-29 21:16:10 +0600

    feat: add cart icon back and add styles for components

--- src/ts/components/Header/Header.ts
+++ src/ts/components/Header/Header.ts
@@ -12,7 +12,7 @@ export class Header {
   unmount = () => {
     this.component?.remove();
   };
-  
+
   mount = () => {
     this.component = create({
       tagName: 'div',
@@ -53,6 +53,24 @@ export class Header {
                   d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
               </svg>
               `
+            })
+          ]
+        }),
+        create({
+          tagName: 'div',
+          classNames: 'cart-shopping__wrapper',
+          children: [
+            create({
+              tagName: 'button',
+              classNames: 'cart-shopping__button',
+              children: `
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+                class="cart-shopping__svg">
+                <path
+                  d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+              </svg>`
             })
           ]
         })
